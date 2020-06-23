6f129d9c5217d906668ac555d6b49fa1a2c4b922
vsmityukh <smityukh@gmail.com>
2020-06-23 17:03:40 +0300

    update 23-06

--- js/common.js
+++ js/common.js
@@ -1,5 +1,46 @@
 $(function() {
 
-	// Custom JS
+	$('.tab_links a').on('click', function(e){
+		e.preventDefault();
+		$(this).closest('.tab_links').find('.active').removeClass('active');
+		$(this).addClass('active');
+		
+		var href = $(this).attr('href');
+		$('.tab_contents .active').removeClass('active');
+		$(href).addClass('active');
+	})
 
-});
+ 
+	$('.top_slider').slick({
+		infinite: false,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		responsive: [
+			{
+			  breakpoint: 1509,
+			  settings: {
+				slidesToShow: 3,
+				slidesToScroll: 1,
+			  }
+			},
+			{
+			  breakpoint: 991,
+			  settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2
+			  }
+			},
+			{
+			  breakpoint: 576,
+			  settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			  }
+			}
+		]
+	})
+
+ 
+	$('.cases').scrollbar();
+	$('audio').initAudioPlayer();
+}); 
